5c2ed721731fc4d15f9c7431b94891c1f0ebb060
Buz <contato@gabrielhenrique.net>
2021-02-26 13:27:42 -0300

    add account data in user route

--- lib/rocketpay_web/controllers/users_controller.ex
+++ lib/rocketpay_web/controllers/users_controller.ex
@@ -7,7 +7,7 @@ defmodule RocketpayWeb.UsersController do
 
   def create(conn, params) do
     with {:ok, %User{} = user} <- Rocketpay.create_user(params) do
-        conn
+      conn
       |> put_status(:created)
       |> render("create.json", user: user)
     end
